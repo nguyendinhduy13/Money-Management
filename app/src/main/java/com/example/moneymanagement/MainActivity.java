744e59437ebed8e37fdcd1a8e96d8cca28b8258c
nguyendinhduy13 <your_email@example.com>
2022-04-01 16:26:14 +0700

    welcome screen

--- app/src/main/java/com/example/moneymanagement/MainActivity.java
+++ app/src/main/java/com/example/moneymanagement/MainActivity.java
@@ -1,14 +1,37 @@
 package com.example.moneymanagement;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.view.View;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import me.relex.circleindicator.CircleIndicator;
 
 public class MainActivity extends AppCompatActivity {
-
+    private ViewPager viewPager;
+    private CircleIndicator circleIndicator;
+    private PhotoAdapter photoAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        viewPager=findViewById(R.id.viewpaper);
+        circleIndicator=findViewById(R.id.circle_indicator);
+        photoAdapter=new PhotoAdapter(this,getListPhoto());
+        viewPager.setAdapter(photoAdapter);
+        circleIndicator.setViewPager(viewPager);
+        photoAdapter.registerDataSetObserver(circleIndicator.getDataSetObserver());
+    }
+    private List<Photo> getListPhoto(){
+        List<Photo> list=new ArrayList<>();
+        list.add(new Photo(R.drawable.image1));
+        list.add(new Photo(R.drawable.image2));
+        list.add(new Photo(R.drawable.image3));
+        list.add(new Photo(R.drawable.image4));
+        return list;
     }
 }
