a1af003296316e78e4ee15305779fb3d977e73b9
nguyendinhduy13 <your_email@example.com>
2022-04-05 22:46:54 +0700

    tự động di chuyển ảnh

--- app/src/main/java/com/example/moneymanagement/MainActivity.java
+++ app/src/main/java/com/example/moneymanagement/MainActivity.java
@@ -4,10 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.view.View;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import me.relex.circleindicator.CircleIndicator;
 
@@ -15,16 +19,20 @@ public class MainActivity extends AppCompatActivity {
     private ViewPager viewPager;
     private CircleIndicator circleIndicator;
     private PhotoAdapter photoAdapter;
+    private List<Photo> mListPhoto;
+    private Timer mTimer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         viewPager=findViewById(R.id.viewpaper);
         circleIndicator=findViewById(R.id.circle_indicator);
-        photoAdapter=new PhotoAdapter(this,getListPhoto());
+        mListPhoto=getListPhoto();
+        photoAdapter=new PhotoAdapter(this,mListPhoto);
         viewPager.setAdapter(photoAdapter);
         circleIndicator.setViewPager(viewPager);
         photoAdapter.registerDataSetObserver(circleIndicator.getDataSetObserver());
+        autoSlideImage();
     }
     private List<Photo> getListPhoto(){
         List<Photo> list=new ArrayList<>();
@@ -33,5 +41,41 @@ public class MainActivity extends AppCompatActivity {
         list.add(new Photo(R.drawable.image3));
         list.add(new Photo(R.drawable.image4));
         return list;
+    }
+    private void autoSlideImage(){
+        if(mListPhoto==null|| mListPhoto.isEmpty()||viewPager==null){
+            return;
+        }
+        if(mTimer==null){
+            mTimer=new Timer();
+        }
+        mTimer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                new Handler(Looper.getMainLooper()).post(new Runnable() {
+                    @Override
+                    public void run() {
+                        int currentItem=viewPager.getCurrentItem();
+                        int totalItem=mListPhoto.size()-1;
+                        if(currentItem<totalItem){
+                            currentItem++;
+                            viewPager.setCurrentItem(currentItem);
+                        }
+                        else{
+                            viewPager.setCurrentItem(0);
+                        }
+                    }
+                });
+            }
+        },500,3000 );
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if(mTimer!=null){
+            mTimer.cancel();
+            mTimer=null;
+        }
     }
 }
