4ec6538143492c97d5d20bf7c498ca4f1c3f641a
BlockBusterk <20520204@gm.uit.edu.vn>
2022-04-26 09:52:43 +0700

    Danh mục

--- app/src/main/java/com/example/moneymanagement/Expense.java
+++ app/src/main/java/com/example/moneymanagement/Expense.java
@@ -3,12 +3,18 @@ package com.example.moneymanagement;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 
 public class Expense extends AppCompatActivity {
-
+String [] myList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_expense);
+        myList= getResources().getStringArray(R.array.items);
+        ArrayAdapter<String> adapter=new ArrayAdapter<>(this,R.layout.drop_down_item,myList);
+        AutoCompleteTextView autoCompleteTextView=findViewById(R.id.filled_exposed);
+        autoCompleteTextView.setAdapter(adapter);
     }
 }
