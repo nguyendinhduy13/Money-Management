b40f6366de75570d28639a239b5650ad8771b8dc
DuyChamHoc <duytran9262002@gmail.com>
2022-04-29 19:14:29 +0700

    giao dịch gần đây

--- app/src/main/java/com/example/moneymanagement/HomeActivity.java
+++ app/src/main/java/com/example/moneymanagement/HomeActivity.java
@@ -2,6 +2,8 @@ package com.example.moneymanagement;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -15,14 +17,30 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
+
+import org.joda.time.DateTime;
+import org.joda.time.Months;
+import org.joda.time.MutableDateTime;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 public class HomeActivity extends AppCompatActivity {
 
     private LinearLayout linearNgansach;
     private TextView tv_homnay,tvthunhap,tv_week,tv_month;
-    private DatabaseReference budgetRef,personalRef;
+    private DatabaseReference budgetRef,personalRef,expensesRef;
     private FirebaseAuth mAuth;
+    private RecyclerView recyclerView;
+
+    private WeekSpendingAdapter weekSpendingAdapter;
+    private List<Data> myDataList;
+    private String onlineUserId = "";
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,8 +50,26 @@ public class HomeActivity extends AppCompatActivity {
         tv_week=findViewById(R.id.tv_week);
         tv_month=findViewById(R.id.tv_month);
         tvthunhap=findViewById(R.id.tv_thunhap);
+        recyclerView=findViewById(R.id.recyclerView);
         mAuth = FirebaseAuth.getInstance();
         budgetRef= FirebaseDatabase.getInstance().getReference().child("budget").child(mAuth.getCurrentUser().getUid());
+
+
+
+        recyclerView = findViewById(R.id.recyclerView);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        linearLayoutManager.setStackFromEnd(true);
+        linearLayoutManager.setReverseLayout(true);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+        onlineUserId = mAuth.getCurrentUser().getUid();
+
+        myDataList = new ArrayList<>();
+        weekSpendingAdapter = new WeekSpendingAdapter(HomeActivity.this, myDataList);
+        recyclerView.setAdapter(weekSpendingAdapter);
+
+        readMonthSpendingItems();
 
         budgetRef.addValueEventListener(new ValueEventListener() {
             @Override
@@ -57,6 +93,9 @@ public class HomeActivity extends AppCompatActivity {
 
             }
         });
+
+
+
 
         linearNgansach.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -86,6 +125,43 @@ public class HomeActivity extends AppCompatActivity {
                 Intent intent = new Intent(HomeActivity.this,WeekSpendingActivity.class);
                 intent.putExtra("type","month");
                 startActivity(intent);
+            }
+        });
+    }
+
+    private void readMonthSpendingItems() {
+        MutableDateTime epoch = new MutableDateTime();
+        epoch.setDate(0);
+        DateTime now = new DateTime();
+        Months months = Months.monthsBetween(epoch, now);
+
+        expensesRef = FirebaseDatabase.getInstance().getReference("expenses").child(onlineUserId);
+
+        Query query = expensesRef.orderByChild("month").equalTo(months.getMonths());
+        query.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                myDataList.clear();
+                for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                    Data data = dataSnapshot.getValue(Data.class);
+                    myDataList.add(data);
+                }
+
+                weekSpendingAdapter.notifyDataSetChanged();
+
+                int totalAmount = 0;
+                for (DataSnapshot ds : snapshot.getChildren()) {
+                    Map<String, Object> map = (Map<String, Object>) ds.getValue();
+                    Object total = map.get("amount");
+                    int pTotal = Integer.parseInt(String.valueOf(total));
+                    totalAmount += pTotal;
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
             }
         });
     }
